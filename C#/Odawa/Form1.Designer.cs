4121b286a76e79970cc718e35a069957f1a45850
denischarette <denis@charette.be>
2015-04-07 21:14:31 +0200

    Création dossiers BU et DAL

--- C#/Odawa/Form1.Designer.cs
+++ C#/Odawa/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(741, 413);
             this.Name = "Form1";
             this.Text = "Odawa";
             this.Load += new System.EventHandler(this.Form1_Load);
