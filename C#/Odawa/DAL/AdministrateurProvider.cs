bfda521d9886cc68e7dafe62fc03f9c3d1650609
Errensuge <errensuge@gmail.com>
2015-04-11 20:53:55 +0200

    Utilisateur Et Administrateur -- DS

--- C#/Odawa/DAL/AdministrateurProvider.cs
+++ C#/Odawa/DAL/AdministrateurProvider.cs
@@ -10,73 +10,130 @@ using Odawa.BU.Entities;
 namespace Odawa.DAL
 {
     class AdministrateurProvider
-    {        
+    {
+        OdawaDS odawa = new OdawaDS();
+
         public List<Administrateur> GetAll()
         {
-            List<Administrateur> list = new List<Administrateur>();
+            List<Administrateur> lst = new List<Administrateur>();
 
-            using (SqlConnection cn = new SqlConnection())
+            using (OdawaDSTableAdapters.administrateursTableAdapter adpt = new OdawaDSTableAdapters.administrateursTableAdapter())
+            { adpt.Fill(odawa.administrateurs); }
+
+            foreach (OdawaDS.administrateursRow userRow in odawa.administrateurs.Rows)
             {
-                cn.ConnectionString = ConfigurationManager.ConnectionStrings["odawaConnectionString"].ConnectionString;
-                cn.Open();
-
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandText = "Select * from [administrateurs]";
-                    cmd.Connection = cn;
-
-                    using (SqlDataReader rdr = cmd.ExecuteReader())
-                    {
-                        while (rdr.Read())
-                        {
-                            Administrateur a = new Administrateur();
-                            a.id = (int) rdr["id"];
-                            a.nom = rdr["nom"].ToString();
-                            a.prenom = rdr["prenom"].ToString();
-                            a.username = rdr["username"].ToString();
-                            a.password = rdr["password"].ToString();
-                            a.email = rdr["email"].ToString();
-                            a.phone = rdr["phone"].ToString();
-                            list.Add(a);
-                        }
-                    }
-                }
+                Administrateur user = new Administrateur();
+                user.id = userRow.id;
+                user.nom = userRow.nom;
+                user.prenom = userRow.prenom;
+                user.username = userRow.username;
+                user.password = userRow.password;
+                user.phone = userRow.phone;
+                lst.Add(user);
             }
-            return list;
+
+            return lst;
         }
 
-        public List<Administrateur> SearchData(string Texte)
+        public List<Administrateur> Search( int id, String nom, String prenom, String username, String password, String phone )
         {
-            List<Administrateur> list = new List<Administrateur>();
+            List<Administrateur> lst = new List<Administrateur>();
 
-            using (SqlConnection cn = new SqlConnection())
+            using (OdawaDSTableAdapters.administrateursTableAdapter adpt = new OdawaDSTableAdapters.administrateursTableAdapter())
+            { adpt.Fill(odawa.administrateurs); }
+
+            foreach (OdawaDS.administrateursRow userRow in odawa.administrateurs.Rows)
             {
-                cn.ConnectionString = ConfigurationManager.ConnectionStrings["odawaConnectionString"].ConnectionString;
-                cn.Open();
-
-                using (SqlCommand cmd = new SqlCommand())
+                if ((id == 0) || (userRow.id == id))
                 {
-                    cmd.CommandText = "SELECT * FROM [administrateurs] WHERE nom LIKE " + "'%" + Texte + "%'";
-                    cmd.Connection = cn;
-
-                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    if ((nom == null) || (userRow.nom == nom))
                     {
-                        while (rdr.Read())
+                        if ((prenom == null) || (userRow.prenom == prenom))
                         {
-                            Administrateur a = new Administrateur();
-                            a.id = (int)rdr["id"];
-                            a.nom = rdr["nom"].ToString();
-                            a.prenom = rdr["prenom"].ToString();
-                            a.username = rdr["username"].ToString();
-                            a.password = rdr["password"].ToString();
-                            a.email = rdr["email"].ToString();
-                            a.phone = rdr["phone"].ToString();
-                            list.Add(a);
+                            if ((username == null) || (userRow.username == username))
+                            {
+                                if ((password == null) || (userRow.password == password))
+                                {
+                                    if ((phone == null) || (userRow.phone == phone))
+                                    {
+                                        Administrateur user = new Administrateur();
+                                        user.id = userRow.id;
+                                        user.nom = userRow.nom;
+                                        user.prenom = userRow.prenom;
+                                        user.username = userRow.username;
+                                        user.password = userRow.password;
+                                        user.phone = userRow.phone;
+                                        lst.Add(user);
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
-            return list;
+
+            return lst;
         }
+
+        public Administrateur Search(int id)
+        {
+            Administrateur user = new Administrateur();
+            List<Administrateur> lst = Search(id, null, null, null, null, null);
+            user = lst.First();
+            return user;
+        }
+
+        /*** Exist() ne retourne donc true que s'il existe un et un seul éléments. 
+             C'est logique pour les utilisateurs ( Vu qu'un username ne peut exister qu'un un seul exemplaire dans la BDD ).
+             Ca ne l'est pas spécialement  pour les autres tables. :)
+        ***/
+
+        public bool Exist(String username, String password)
+        {
+            bool exist = false;
+            List<Administrateur> lst = new List<Administrateur>();
+            lst = Search(0, null, null, username, password, null);
+            exist = (lst.Count == 1) ? true : false;
+            return exist;
+        }
+
+        public void Create(String nom, String prenom, String username, String password, String phone)
+        {
+            OdawaDS.administrateursRow newUserRow = odawa.administrateurs.NewadministrateursRow();
+
+            newUserRow.nom = nom;
+            newUserRow.prenom = prenom;
+            newUserRow.username = username;
+            newUserRow.password = password;
+            newUserRow.phone = phone;
+            odawa.administrateurs.Rows.Add(newUserRow);
+
+            odawa.administrateurs.AcceptChanges();
+        }
+
+        public void Update(int id, String nom, String prenom, String username, String password, String phone)
+        {
+            OdawaDS.administrateursRow updUserRow = odawa.administrateurs.FindByid(id);
+
+            if (nom != null)
+                updUserRow.nom = nom;
+            if (prenom != null)
+                updUserRow.prenom = prenom;
+            if (username != null)
+                updUserRow.username = username;
+            if (password != null)
+                updUserRow.password = password;
+            if (phone != null)
+                updUserRow.phone = phone;
+
+            odawa.administrateurs.AcceptChanges();
+        }
+
+        public void Delete(int id)
+        {
+            odawa.administrateurs.FindByid(id).Delete();
+            odawa.administrateurs.AcceptChanges();
+        }
+
     }
 }
--- C#/Odawa/DAL/UtilisateurProvider.cs
+++ C#/Odawa/DAL/UtilisateurProvider.cs
@@ -5,44 +5,138 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using Odawa.BU.Entities;
+
 
 namespace Odawa.DAL
 {
     class UtilisateurProvider
     {
+        OdawaDS odawa = new OdawaDS();
+
         public List<Utilisateur> GetAll()
         {
-            List<Utilisateur> list = new List<Utilisateur>();
+            List<Utilisateur> lst = new List<Utilisateur>();
 
-            using (SqlConnection cn = new SqlConnection())
+            using( OdawaDSTableAdapters.utilisateursTableAdapter adpt = new OdawaDSTableAdapters.utilisateursTableAdapter() )
+            { adpt.Fill(odawa.utilisateurs); }
+
+            foreach (OdawaDS.utilisateursRow userRow in odawa.utilisateurs.Rows)
             {
-                cn.ConnectionString = ConfigurationManager.ConnectionStrings["odawaConnectionString"].ConnectionString;
-                cn.Open();
+                Utilisateur user = new Utilisateur();
+                user.id = userRow.id;
+                user.nom = userRow.nom;
+                user.prenom = userRow.prenom;
+                user.username = userRow.username;
+                user.password = userRow.password;
+                user.phone = userRow.phone;
+                lst.Add(user);
+            }
 
-                using (SqlCommand cmd = new SqlCommand())
+            return lst;
+        }
+
+
+        public List<Utilisateur> Search( int id, String nom, String prenom, String username, String password, String phone )
+        {
+            List<Utilisateur> lst = new List<Utilisateur>();
+
+            using (OdawaDSTableAdapters.utilisateursTableAdapter adpt = new OdawaDSTableAdapters.utilisateursTableAdapter())
+            { adpt.Fill(odawa.utilisateurs); }
+
+            foreach (OdawaDS.utilisateursRow userRow in odawa.utilisateurs.Rows)
+            {
+                if( ( id == 0 ) || ( userRow.id == id ) )
                 {
-                    cmd.CommandText = "Select * from [utilisateurs]";
-                    cmd.Connection = cn;
-
-                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    if( ( nom == null ) || ( userRow.nom == nom ) )
                     {
-                        while (rdr.Read())
+                        if( ( prenom == null ) || ( userRow.prenom == prenom ) )
                         {
-                            Utilisateur u = new Utilisateur();
-                            u.id = (int)rdr["id"];
-                            u.nom = rdr["nom"].ToString();
-                            u.prenom = rdr["prenom"].ToString();
-                            u.username = rdr["username"].ToString();
-                            u.password = rdr["password"].ToString();
-                            u.email = rdr["email"].ToString();
-                            u.phone = rdr["phone"].ToString();
-                            list.Add(u);
+                            if( ( username == null ) || ( userRow.username == username ) )
+                            {
+                                if( (password == null ) || ( userRow.password == password ) )
+                                {
+                                   if( (phone == null ) || ( userRow.phone == phone ) )
+                                   {
+                                       Utilisateur user = new Utilisateur();
+                                       user.id = userRow.id;
+                                       user.nom = userRow.nom;
+                                       user.prenom = userRow.prenom;
+                                       user.username = userRow.username;
+                                       user.password = userRow.password;
+                                       user.phone = userRow.phone;
+                                       lst.Add(user);
+                                   }
+                                }
+                            }
                         }
                     }
                 }
             }
-            return list;
+
+            return lst;
         }
+
+        public Utilisateur Search( int id )
+        {
+            Utilisateur user = new Utilisateur();
+            List<Utilisateur> lst = Search(id, null, null, null, null, null);
+            user = lst.First();
+            return user;
+        }
+
+        /*** Exist() ne retourne donc true que s'il existe un et un seul éléments. 
+             C'est logique pour les utilisateurs ( et les administrateurs ) ( Vu qu'un username ne peut exister qu'un un seul exemplaire dans la BDD ).
+             Ca ne l'est pas spécialement  pour les autres tables. :)
+        ***/
+
+        public bool Exist( String username, String password )
+        {
+            bool exist = false;
+            List<Utilisateur> lst = new List<Utilisateur>();
+            lst = Search(0, null, null, username, password, null);
+            exist = (lst.Count == 1) ? true : false ;
+            return exist;
+        }
+
+        public void Create( String nom, String prenom, String username, String password, String phone )
+        {
+            OdawaDS.utilisateursRow newUserRow = odawa.utilisateurs.NewutilisateursRow();
+
+            newUserRow.nom = nom;
+            newUserRow.prenom = prenom;
+            newUserRow.username = username;
+            newUserRow.password = password;
+            newUserRow.phone = phone;
+            odawa.utilisateurs.Rows.Add(newUserRow);
+
+            odawa.utilisateurs.AcceptChanges();
+        }
+
+        public void Update( int id, String nom, String prenom, String username, String password, String phone )
+        {
+            OdawaDS.utilisateursRow updUserRow = odawa.utilisateurs.FindByid(id);
+
+            if (nom != null)
+                updUserRow.nom = nom;
+            if (prenom != null)
+                updUserRow.prenom = prenom;
+            if (username != null)
+                updUserRow.username = username;
+            if (password != null)
+                updUserRow.password = password;
+            if (phone != null)
+                updUserRow.phone = phone;
+
+            odawa.utilisateurs.AcceptChanges();
+        }
+
+        public void Delete( int id )
+        {
+            odawa.utilisateurs.FindByid(id).Delete();
+            odawa.utilisateurs.AcceptChanges();
+        }
+
     }
 }
